7828294665d7c8734d3ed443585028228a3a4c16
Alina Miao <amiao00@berkeley.edu>
2021-08-03 16:48:23 -0700

    updated CSS, pen and eraser functions, added a grid size input slider and function

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-const gridContainer = document.querySelector("#grid-container");
-createCanvas();
-etch();
+const gridContainer = document.getElementById("grid-container");
+const sizeSelector = document.getElementById("size-selector");
+const gridItems = document.getElementsByClassName("grid-item");
+const penColor = document.getElementById("pen-color");
 
-//creates a 16x16 grid
+createCanvas();
+
+//creates a grid for the canvas, default size 16x16
 function createCanvas() {
-	for (let i = 0; i < 256; i++) {
+	gridContainer.style.gridTemplateColumns = `repeat(${sizeSelector.value}, 1fr)`;
+	gridContainer.style.gridTemplateRows = `repeat(${sizeSelector.value}, 1fr)`;
+	for (let i = 0; i < sizeSelector.value * sizeSelector.value; i++) {
 		const gridItem = document.createElement("div");
 		gridItem.classList.add("grid-item")
 		gridContainer.appendChild(gridItem);
 	};
 };
 
-//adds event listeners on all grid items to change color
-function etch() {
-	const gridItems = document.getElementsByClassName("grid-item");
-	const penColor = document.getElementById("pen-color");
+//size selector
+sizeSelector.addEventListener("change", function() {
+	gridContainer.innerHTML = "";
+	createCanvas();
+	etch();
+})
 
+//pen color and drawing
+function etch() {
 	for (let i = 0; i < gridItems.length; i++) {
 	gridItems[i].addEventListener("mousemove", function(event) {
 		event.target.style.backgroundColor = penColor.value;
@@ -23,16 +32,41 @@ function etch() {
 	};
 };
 
+const penButton = document.getElementById("pen-button");
+penButton.addEventListener("click", function(event) {
+	event.target.style.cssText = `background-color: #000000; color: #ffffff;`;
+	eraserButton.style.cssText = `background-color: #ffffff; color: #000000;`;
+	etch();
+})
+
+//eraser function
+
+function erase() {
+	for (let i = 0; i < gridItems.length; i++) {
+		gridItems[i].addEventListener("mousemove", function(event) {
+			event.target.style.backgroundColor = "#ffffff";
+		});
+	};
+};
+
+const eraserButton = document.getElementById("eraser-button");
+eraserButton.addEventListener("click", function(event) {
+	event.target.style.cssText = `background-color: #000000; color: #ffffff;`;
+	penButton.style.cssText = `background-color: #ffffff; color: #000000;`;
+	erase();
+});
+
 //clears the Canvas
 const clearButton = document.getElementById("clear-button");
 clearButton.addEventListener("click", function() {
+	penButton.style.cssText = `background-color: #ffffff; color: #000000;`;
+	eraserButton.style.cssText = `background-color: #ffffff; color: #000000;`;
 	gridContainer.innerHTML = "";
 	createCanvas();
-	etch();
 });
 
 
 /*
-Still need to set up grid customization and better hover/drawing and CSS styling
+Still need to set up better hover/drawing and eraser and css
 https://levelup.gitconnected.com/build-an-etch-a-sketch-knock-off-with-plain-js-css-and-html-9ab9e104b43f
 */
